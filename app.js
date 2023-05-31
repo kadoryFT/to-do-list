f70e0270b10c697b7b5760bf5b0ebefd1011da71
kadoryFT <72969287+kadoryFT@users.noreply.github.com>
2023-05-31 23:47:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+let items =["buy food ","make food","eat food"];
+
+app.set("view engine", "ejs");
+
+app.use(express.static("puplic"));
+app.use(express.urlencoded());
+
+
+app.get("/", function(req, res) {
+
+   let today = new Date();
+
+   let options = {
+      weekday : "long",
+      day: "numeric",
+      month :"long"
+   };
+ let day =today.toLocaleDateString("en-US", options);
+ 
+   res.render("list", { kindOfDay: day  , newListItems : items});  
+});
+
+app.post("/" ,function(req ,res){
+   let item =req.body.newItem;
+   items.push(item);
+   res.redirect("/");
+   })
+
+app.listen(3001, function() {
+   console.log("yay");
+});
